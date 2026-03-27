a5a1507e0b3c456f8039dc7ed98240e578cf3d30
Puleesha <puleeshak@gmail.com>
2026-03-27 15:17:18 +0530

    Simplify Prometheus server

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,12 @@ use request_handler::RequestHandler;
 use tool_service::ToolService;
 
 use metrics::{counter, histogram};
-use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
+use metrics_exporter_prometheus::PrometheusBuilder;
 
 use std::env;
 use std::time::{Instant, Duration};
 use std::thread;
 use std::sync::Arc;
-use std::convert::Infallible;
-
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
-use hyper::header::CONTENT_TYPE;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -38,12 +33,12 @@ async fn main() -> Result<()> {
     // -----------------------------
     // Install Prometheus exporter ONCE
     // -----------------------------
-    let handle: PrometheusHandle = PrometheusBuilder::new()
-        .install_recorder()
+    PrometheusBuilder::new()
+        .with_http_listener(([0, 0, 0, 0], port))
+        .install()
         .expect("failed to install recorder");
 
     eprintln!("Listening on port {}", port);    // Print all logs into stderr
-    start_metrics_server(handle.clone(), port);
 
     // -----------------------------
     // BENCHMARK MODE
@@ -101,54 +96,3 @@ async fn main() -> Result<()> {
     // Similar to void return type in Java
     Ok(())
 }
-
-// -----------------------------
-// Make metrics response headers work with Prometheus.
-// -----------------------------
-async fn metrics_handler(
-    req: Request<Body>,
-    handle: PrometheusHandle,
-) -> Result<Response<Body>, Infallible> {
-
-    if req.uri().path() != "/metrics" {
-        return Ok(Response::builder()
-            .status(404)
-            .body(Body::from("Not Found"))
-            .unwrap());
-    }
-
-    let body = handle.render();
-
-    Ok(Response::builder()
-        .status(200)
-        .header(
-            CONTENT_TYPE,
-            "text/plain; version=0.0.4; charset=utf-8"
-        )
-        .body(Body::from(body))
-        .unwrap())
-}
-
-fn start_metrics_server(handle: PrometheusHandle, port: u16) {
-    tokio::spawn(async move {
-        let addr = ([0, 0, 0, 0], port).into();
-
-        let make_svc = make_service_fn(move |_conn| {
-            let h = handle.clone();
-            async move {
-                Ok::<_, Infallible>(service_fn(move |req| {
-                    metrics_handler(req, h.clone())
-                }))
-            }
-        });
-
-        let server = Server::bind(&addr).serve(make_svc);
-
-        eprintln!("Metrics server on http://0.0.0.0:{}/metrics", port);
-
-        if let Err(e) = server.await {
-            eprintln!("metrics server error: {}", e);
-        }
-    });
-}
-
